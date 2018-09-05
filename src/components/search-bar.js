ab23f32c7915abedcc502f2f6d3456858d7f8a9d
vab <vincent.alibert@fullsave.com>
2018-09-05 16:40:40 +0200

    ajout des premiers composants

---
+++ src/components/search-bar.js
@@ -0,0 +1,5 @@
+import React,{component} from 'react';
+
+const SearchBar = () => <input/>;
+
+export default SearchBar;
---
+++ src/containers/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import SearchBar from '../components/search-bar';
+
+const App = () => <div><SearchBar/></div>
+
+export default App;
